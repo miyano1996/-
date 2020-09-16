bf260fe2580bd45272fe6f9feef6851ec03a9371
YangDehui <yangdehui8842@foxmail.com>
2020-09-16 23:01:13 +0800

    基础页面更新

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import './App.css';
-// import Login from './pages/admin/Login'
-// import Res from './pages/admin/Res'
 import Home from './pages/Home'
 function App() {
   return (
