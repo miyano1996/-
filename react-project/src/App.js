427d021fbb483f634113cb5ddcfc4752ab88bf35
YangDehui <yangdehui8842@foxmail.com>
2020-09-16 22:44:45 +0800

    新增了 antd、路由、路由懒加载、redux、react-redux等 api；新增了大致布局

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import Login from './pages/admin/Login'
-import Res from './pages/admin/Res'
-
+// import Login from './pages/admin/Login'
+// import Res from './pages/admin/Res'
+import Home from './pages/Home'
 function App() {
   return (
     <>
-    <Login />
-    <Res/>
+      <Home />
     </>
   );
 }
