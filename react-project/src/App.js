4ea8afecf7ea6628cbebedec1393048927699a03
WenHao <wenhao@example.com>
2020-09-16 19:15:14 +0800

    test

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 import Login from './pages/admin/Login'
+import Res from './pages/admin/Res'
+
 function App() {
   return (
+    <>
     <Login />
+    <Res/>
+    </>
   );
 }
 
