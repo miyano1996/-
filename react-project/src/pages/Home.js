72b277f58d821e4dcbf79c7f9075f7fe2493bd91
XueYanBo <739233662@qq.com>
2020-09-17 11:33:47 +0800

    添加scss

--- react-project/src/pages/Home.js
+++ react-project/src/pages/Home.js
@@ -9,48 +9,53 @@ const Login = Loadable({
     loader: () => import('./admin/Login.js'),
     loading: () => <div>加载中...</div>
 })
+const Complaint = Loadable({
+    loader: () => import('./manage/Complaint'),
+    loading: () => <div>加载中...</div>
+})
 
 const { Header, Content } = Layout;
 
 export default class Home extends Component {
     render() {
         return (
+            <Layout>
+                <Header className="header">
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                        <Menu.Item key="2">nav 2</Menu.Item>
+                        <Menu.Item key="3">nav 3</Menu.Item>
+                    </Menu>
+                </Header>
                 <Layout>
-                    <Header className="header">
-                        <div className="logo" />
-                        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                            <Menu.Item key="1">nav 1</Menu.Item>
-                            <Menu.Item key="2">nav 2</Menu.Item>
-                            <Menu.Item key="3">nav 3</Menu.Item>
-                        </Menu>
-                    </Header>
-                    <Layout>
-                        <SideBar />
-                        <Layout style={{ padding: '0 24px 24px' }}>
-                            <Breadcrumb style={{ margin: '16px 0' }}>
-                                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                                <Breadcrumb.Item>List</Breadcrumb.Item>
-                                <Breadcrumb.Item>App</Breadcrumb.Item>
-                            </Breadcrumb>
-                            <Content
-                                className="site-layout-background"
-                                style={{
-                                    padding: 24,
-                                    margin: 0,
-                                    minHeight: 280,
-                                }}
-                            >
-                                <HashRouter>
-                                    <Switch>
-                                        <Redirect exact path="/" to="/home/login"></Redirect>
-                                        <Route path="/home/login" component={Login}></Route>
-                                    </Switch>
-                                </HashRouter>
-                            </Content>
+                    <SideBar />
+                    <Layout style={{ padding: '0 24px 24px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>List</Breadcrumb.Item>
+                            <Breadcrumb.Item>App</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <Content
+                            className="site-layout-background"
+                            style={{
+                                padding: 24,
+                                margin: 0,
+                                minHeight: 280,
+                            }}
+                        >
+                            <HashRouter>
+                                <Switch>
+                                    <Redirect exact path="/" to="/home/Complaint"></Redirect>
+                                    <Route path="/home/login" component={Login}></Route>
+                                    <Route path="/home/Complaint" component={Complaint}></Route>
+                                </Switch>
+                            </HashRouter>
+                        </Content>
 
-                        </Layout>
                     </Layout>
                 </Layout>
+            </Layout>
         )
     }
 }
