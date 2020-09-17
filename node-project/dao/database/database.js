d2ee6fa21d4023a31de7da93f7593cbed25e95e0
gao_minchun <916582220@qq.com>
2020-09-17 10:33:42 +0800

    数据库迁移至服务器

--- node-project/dao/database/database.js
+++ node-project/dao/database/database.js
@@ -1,7 +1,10 @@
 // 连接数据库
 const mongoose = require('mongoose');
 // 设置需要连接的数据库地址
-const dbURI = 'mongodb://localhost/fitness';
+const dbURI = 'mongodb://111.231.189.144:27017/fitness';
+//若需连接本地可以把路径设置:可选路径
+//111.231.189.144:27017
+//localhost
 // 连接数据库
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 // 当数据库连接成功后触发该事件
