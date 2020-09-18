d9d7ef2fc5c89cc13d535e8fe40a0962b2e3a88e
zjf <yll952@haha.com>
2020-09-18 16:45:59 +0800

    123

--- react-project/src/pages/Home.js
+++ react-project/src/pages/Home.js
@@ -17,34 +17,20 @@ const Orders = Loadable({
     loader: () => import('./orders/Orders'),
     loading: () => <div>加载中...</div>
 })
-const Sdetails = Loadable({
-    loader: () => import('./list/Sdetails'),
-    loading: () => <div>加载中...</div>
-})
-const Cdetails = Loadable({
-    loader: () => import('./list/Cdetails'),
-    loading: () => <div>加载中...</div>
-})
 const Studentslist = Loadable({
     loader: () => import('./list/Studentlist'),
     loading: () => <div>加载中...</div>
 })
-const Coacheslist = Loadable({
-    loader: () => import('./list/Coacheslist'),
-    loading: () => <div>加载中...</div>
-})
-const Venuelist = Loadable({
-    loader: () => import('./list/Venuelist'),
-})
+
 //申请场馆
 const AddGyms = Loadable({
     loader: () => import('./AddGyms'),
     loading: () => <div>加载中...</div>
 })
 
-//申请场馆
-// const AddGyms = Loadable({
-//     loader: () => import('./AddGyms'),
+//管理员审核
+// const AdminReview = Loadable({
+//     loader: () => import('./AdminReview'),
 //     loading: () => <div>加载中...</div>
 // })
 
@@ -79,24 +65,19 @@ export default class Home extends Component {
                         >
                             <HashRouter>
                                 <Switch>
-                                    <Redirect exact path="/home" to="/home/Coacheslist"></Redirect>
-                                    {/* <Route path="/home/login" component={Login}></Route> */}
+                                    <Redirect exact path="/home" to="/home/oneGym"></Redirect>
+                                    <Route path="/home/oneGym" component={OneGym}></Route>
                                     <Route path="/home/Complaint" component={Complaint}></Route>
                                     <Route path="/home/Studentslist" component={Studentslist}></Route>
-                                    <Route path="/home/Coacheslist" component={Coacheslist}></Route>
                                     <Route path="/home/Orders" component={Orders}></Route>
-                                    <Route path="/home/Coacheslist" component={Coacheslist}></Route>
-                                    <Route path="/home/Venuelist" component={Venuelist}></Route>
-                                    <Route path="/home/Sdetails" component={Sdetails}></Route>
-                                    <Route path="/home/Cdetails" component={Cdetails}></Route>
                                     <Route path="/home/addGyms" component={AddGyms}></Route>
-                                    <Route path="/home/oneGym" component={OneGym}></Route>
+                                    {/* <Route path="/home/adminReview" component={AdminReview}></Route> */}
                                 </Switch>
                             </HashRouter>
                         </Content>
                     </Layout>
                 </Layout>
             </Layout>
-    );
-  }
+        )
+    }
 }
