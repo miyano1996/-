96ddcc9ea389d18ffb4ffacc7c60ed29ce7bcc18
gao_minchun <916582220@qq.com>
2020-09-21 10:12:17 +0800

    注册学员

--- node-project/routes/students.js
+++ node-project/routes/students.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-const { getOne } = require('../service/studentsService')
+const { getOne,reg } = require('../service/studentsService')
 
 router.post('/details',async (req,res,next)=>{
   const data =await getOne(req.body)
   res.send(data)
 })
+
+router.post('/reg',async (req,res)=>{
+  const data = await reg(req.body);
+  res.send(data);
+})  
 
 module.exports = router;
