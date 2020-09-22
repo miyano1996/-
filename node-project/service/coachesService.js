722a1f6224d7ac2e9ba758710651fd7159ff0324
gao_minchun <916582220@qq.com>
2020-09-22 19:20:00 +0800

    教练注册

--- node-project/service/coachesService.js
+++ node-project/service/coachesService.js
@@ -1,5 +1,5 @@
 
-const { getCoaches, delCoaches, getOne, updateCoaches, addCoach } = require("../dao/coachesDao");
+const { getCoaches, delCoaches, getOne, updateCoaches, addCoach,reg } = require("../dao/coachesDao");
 
 //获取教练
 module.exports.getCoaches = async function (data) {
@@ -48,4 +48,19 @@ module.exports.addCoach = async (data) => {
     // 后端返回处理结果给前端
     return { success: true, msg: "获取教练成功", rows };
 
+}
+
+//教练注册
+module.exports.reg = async data => {
+	const isReg = await isExist(data);
+	if (isReg.length > 0) {
+		return { success: false, msg: "该用户名已被注册" };
+	} else {
+		const obj = await reg(data);
+		// console.log(obj);
+		if (obj._id) {
+			return { success: true, msg: "注册成功", obj };
+		}
+		return { success: false, msg: "注册失败", obj };
+	}
 }
