919d0d437b6be15b372e0f166a352ae554b7c5e2
gao_minchun <916582220@qq.com>
2020-09-22 19:10:19 +0800

    轮播图和个人接口

--- node-project/service/carouselService.js
+++ node-project/service/carouselService.js
@@ -1,4 +1,4 @@
-const {upCarousel} = require('../dao/carouselDao');
+const {upCarousel,getAllCarousel} = require('../dao/carouselDao');
 
 module.exports.upCarousel = async data=>{
     const obj = await upCarousel(data);
@@ -6,4 +6,9 @@ module.exports.upCarousel = async data=>{
         return {success:true,msg:'上传成功',rows:obj}
     }
     return {success:false,msg:'上传失败',rows:obj}
+}
+
+module.exports.getAllCarousel = async ()=>{
+    const obj = await getAllCarousel();
+    return {success:true,msg:'获取成功',rows:obj}
 }
